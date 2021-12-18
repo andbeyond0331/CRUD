e26994dc9f61325a33ca0f89b49b5bc29ad786a7
pmj <mjpark1329@naver.com>
2021-12-18 13:52:00 +0900

    21.12.18 Menu, Review -> dao, daoImpl, Service, ServiceImpl 메소드 작성 및 menu Controller 작성 시작

--- src/main/java/com/crud/ffin/service/menu/MenuDao.java
+++ src/main/java/com/crud/ffin/service/menu/MenuDao.java
@@ -1,4 +1,32 @@
 package com.crud.ffin.service.menu;
 
+import com.crud.ffin.common.Search;
+import com.crud.ffin.service.domain.Menu;
+import com.crud.ffin.service.domain.OptionGroup;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+
 public interface MenuDao {
+
+    //INSERT
+    public void addMenu(Menu menu) throws Exception;
+
+    //INSERT
+    public void addOptionGroup(OptionGroup optionGroup) throws Exception;
+
+    //SELECT ONE
+    public Menu getMenu(int menuNo) throws Exception;
+
+    //SELECT LIST
+    public List<Menu> getMenuList(Search search) throws Exception;
+
+    //UPDATE
+    public void updateMenu(Menu menu) throws Exception;
+
+    //UPDATE
+    public void updateOptionGroup(OptionGroup optionGroup) throws Exception;
+
+    // 게시판 page 처리를 위한 전체 Row(totalCount) return
+    public int getTotalCount(Search search) throws Exception;
 }
--- src/main/java/com/crud/ffin/service/menu/MenuService.java
+++ src/main/java/com/crud/ffin/service/menu/MenuService.java
@@ -1,4 +1,22 @@
 package com.crud.ffin.service.menu;
 
+import com.crud.ffin.common.Search;
+import com.crud.ffin.service.domain.Menu;
+
+import java.util.Map;
+
 public interface MenuService {
+
+    //메뉴 등록
+    public void addMenu(Menu menu) throws Exception;
+
+    //메뉴 조회
+    public Menu getMenu(int menuNo) throws Exception;
+
+    //메뉴 목록 조회
+    public Map<String, Object> getMenuList(Search search) throws Exception;
+
+    //메뉴 수정
+    public void updateMenu(Menu menu) throws Exception;
+
 }
