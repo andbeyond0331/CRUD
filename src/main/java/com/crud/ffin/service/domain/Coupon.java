49b9828ab98fe847e78601a24cfe48113f61aaba
성원 박 <tjddnjs8503@naver.com>
2021-12-18 13:53:06 +0900

    21.12.18
    Coupon Domain 전체수정
    Purchase Domain 케이터링 서비스 추가수정
    PurchaseDao,Impl,sevice,Impl 수정
    PurchaseMapper 수정

--- src/main/java/com/crud/ffin/service/domain/Coupon.java
+++ src/main/java/com/crud/ffin/service/domain/Coupon.java
@@ -4,30 +4,14 @@ import java.sql.Timestamp;
 import lombok.Data;
 import lombok.Getter;
 
+@Data
 public class Coupon {
-	private String flightId; //flight_id
-	private String airline; //airline
-	private int price; //price
-	private String tripCourse; //trip_course
-	private String depCity; //dep_city
-	private String depDate; //dep_date
-	private String depTime; //dep_time
-	private String arrCity; //arr_city
-	private String arrDate; //arr_date
-	private String arrTime;	//arr_time
-	private String seatGrade; //seat_grade
-	private int adultNum; //adult_num
-	private int childNum; //child_num
-	private int infantNum; //infant_num
-	private String leadTime; //lead_time
-	private String stopOver; //stop_over
-	private String orderStatus; //order_status
-	private Timestamp orderDate; //order_date
-	private String refundDate; 
-	
-	private String orderId;
-	private String buyerId;
-	
+	private int couponNo; //flight_id
+	private String couponReceivedUserId; //airline
+	private int couponDcPrice; //price
+	private int  couponStatus; //trip_course
+	private int couponWeatherType; //dep_city
+
 	
 	
 
--- src/main/java/com/crud/ffin/web/purchase/PurchaseController.java
+++ src/main/java/com/crud/ffin/web/purchase/PurchaseController.java
@@ -1,4 +1,34 @@
 package com.crud.ffin.web.purchase;
 
+import com.crud.ffin.service.domain.Purchase;
+import com.crud.ffin.service.purchase.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/purchase/*")
 public class PurchaseController {
+
+    ///Field
+    @Autowired
+    @Qualifier("purchaseServiceImpl")
+    private PurchaseService purchaseService;
+    //setter Method 구현 않음
+
+    public PurchaseController() {
+        System.out.println(this.getClass());
+    }
+
+
+
+
+
+
 }
